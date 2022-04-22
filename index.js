32a281ebc9080c5a665323578b28e3e7a50cac4b
FelixPlayz <76107024+FelixPlayz@users.noreply.github.com>
2022-04-22 09:14:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 app.get('/',(req, res) => res.sendFile(__dirname+'/index.html'))
 app.use("/css", express.static(__dirname + "/css"));
 
-app.get('/invite', (req, res) => res.redirect('https://discord.com/oauth2/authorize?client_id=876828669522374696&scope=bot%20applications.commands&permissions=805314622')
+app.get('/invite', (req, res) => res.redirect('https://discord.com/oauth2/authorize?client_id=876828669522374696&scope=applications.commands%20bot&permissions=27648860222')
 
     )
     
